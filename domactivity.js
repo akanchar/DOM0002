cb6c00213c3412ad3184e36d1beb9035695c73fe
Anthony <anthony.eccleston@live.com>
2024-09-10 15:47:57 -0400

    Fix - fixed path from thumbnails to images/large

--- domactivity.js
+++ domactivity.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // Loop through the paintings array and create thumbnail images
     paintingsData.forEach(painting => {
         const listItem = document.createElement('li'); // Creates a new <li> element
-        listItem.innerHTML = `<img src="thumbnails/${painting.id}.jpg" data-id="${painting.id}" alt="${painting.title}" class="thumbnail">`; // Adds thumbnail images with the corresponding data-id
+        listItem.innerHTML = `<img src="images/large/${painting.id}.jpg" data-id="${painting.id}" alt="${painting.title}" class="thumbnail">`; // Adds thumbnail images with the corresponding data-id
         paintingList.appendChild(listItem); // Appends the <li> to the <ul> element
     });
  
