e8968ee4a3cb71f0b0c6b072476af6d2324a0a09
NathanFrait <nathaniel.frait@spartans.ut.edu>
2024-09-11 08:42:36 -0400

    [FEAT] - added list to hold image ids

--- domactivity.js
+++ domactivity.js
@@ -1,1 +1,12 @@
 /* add your code here */
+
+addEventListener("DOMContentLoaded", (event) => {
+    var information = JSON.parse(content);
+    var ids = []
+    console.log(information);
+    console.log(information[0].id);
+    for (i = 0; i<information.length; i++) {
+        ids[i] = information[i].id
+    };
+    console.log(ids)
+});
