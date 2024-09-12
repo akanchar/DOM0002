be671d93e7920001b5ffe003f20e7fb796c36a0b
shrncr <loveyawink@gmail.com>
2024-09-12 14:07:41 -0500

    FEAT - rectangles appear over images

--- domactivity.js
+++ domactivity.js
@@ -6,28 +6,44 @@ document.addEventListener("DOMContentLoaded", (event) => {
     artist = document.getElementById("artist");
 
     const data = JSON.parse(content);
+    arr = []
 
     ul = document.querySelector("ul");
+    let count = 0;
 
     data.forEach(item => {
         l =document.createElement("li");
-        
+        arr.push(item);
         i = document.createElement('img');
-        i.id = item.id;
+        i.id = count;
         i.src = "../inclassdomactivity002/images/small/" + item.id + ".jpg";
         
         l.appendChild(i)
         ul.appendChild(l)
+        count++
         
     });
-
+    console.log(arr);
     ul.addEventListener("click", (e)=>{
         console.log(e.target.nodeName);
         if (e.target && e.target.nodeName == "IMG"){
             console.log("lalala")
             //document.querySelector("figure").innerHTML = "";
+            title.innerHTML = arr[e.target.id].title
+            artist.innerHTML = arr[e.target.id].artist
+            fullPic.src = "../inclassdomactivity002/images/large/" + arr[e.target.id].id + ".jpg";
             
-            fullPic.src = "../inclassdomactivity002/images/large/" + e.target.id + ".jpg";
+            arr[e.target.id].features.forEach(feat =>{
+                const rect = document.createElement("div");
+                document.querySelector("figure").appendChild(rect);
+                rect.className ="box";
+                rect.style.position = "absolute";
+                console.log(feat.upperLeft[0])
+                rect.style.left = feat.upperLeft[0] + "px";
+                rect.style.top = feat.upperLeft[1] + "px";
+                rect.style.height =  feat.lowerRight[1] - feat.upperLeft[1] + "px";
+                rect.style.width = feat.lowerRight[0] - feat.upperLeft[0] + "px";
+            });
             
             
         }
