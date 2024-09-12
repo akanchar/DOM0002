cc6a25d3744fefbc2ab3d1e0892aa7c01f16964c
shrncr <loveyawink@gmail.com>
2024-09-12 13:03:53 -0500

    FEAT - sidebar pictures and main pic changed when you change side pictures

--- domactivity.js
+++ domactivity.js
@@ -1,1 +1,38 @@
 /* add your code here */
+
+document.addEventListener("DOMContentLoaded", (event) => {
+    fullPic = document.getElementById("full");
+    title = document.getElementById("title");
+    artist = document.getElementById("artist");
+
+    const data = JSON.parse(content);
+
+    ul = document.querySelector("ul");
+
+    data.forEach(item => {
+        l =document.createElement("li");
+        
+        i = document.createElement('img');
+        i.id = item.id;
+        i.src = "../inclassdomactivity002/images/small/" + item.id + ".jpg";
+        
+        l.appendChild(i)
+        ul.appendChild(l)
+        
+    });
+
+    ul.addEventListener("click", (e)=>{
+        console.log(e.target.nodeName);
+        if (e.target && e.target.nodeName == "IMG"){
+            console.log("lalala")
+            //document.querySelector("figure").innerHTML = "";
+            
+            fullPic.src = "../inclassdomactivity002/images/large/" + e.target.id + ".jpg";
+            
+            
+        }
+    })
+
+  });
+  
+  
