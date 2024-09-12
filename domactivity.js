7996b9daad9e86b7298ffdcc78ab6f1286ea8320
Michelle-l33 <michelle.lee@spartans.ut.edu>
2024-09-12 11:27:26 -0400

    [FIX] - refining handler

--- domactivity.js
+++ domactivity.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
     const jsondata = JSON.parse(content);
     const paintinglist = document.querySelector('#paintings ul'); //this gets the list element under the paintings id section of html
     function thumbnails(){
-        for(element of jsondata){ //for every element in the data array...
+        for(let element of jsondata){ //for every element in the data array...
             const listitem = document.createElement('li'); //creates a list item to go into the list above; got help from https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
             const image = document.createElement('img');
             image.src = "../images/small/${element.id}.jpg";
